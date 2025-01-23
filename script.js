023227d76901190e832ebf2e410bcc46978a347f
Nicola Vettone <nicolavettone57@gmail.com>
2025-01-23 16:17:28 +0100

    button load

--- script.js
+++ script.js
@@ -16,29 +16,37 @@ fetch("https://api.pexels.com/v1/search?query=hamsters", {
     const row = document.querySelector("#row");
     console.log(photosObj.photos);
 
-    photosObj.photos.forEach((photo) => {
-      const col = document.createElement("div");
-      col.classList.add("col-md-4");
-      console.log(photo.src.large);
+    const loadImages = document.querySelector("#load");
 
-      col.innerHTML = ` <div class="card mb-4 shadow-sm">
-                <img src=${photo.src.large} class="bd-placeholder-img card-img-top" />
-                <div class="card-body">
-                  <h5 class="card-title">Lorem Ipsum</h5>
-                  <p class="card-text">
-                    This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.
-                  </p>
-                  <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                      <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                      <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+    loadImages.onclick = function () {
+      loadPhoto();
+    };
+
+    function loadPhoto() {
+      photosObj.photos.forEach((photo) => {
+        const col = document.createElement("div");
+        col.classList.add("col-md-4");
+        console.log(photo.src.large);
+
+        col.innerHTML = ` <div class="card mb-4 shadow-sm">
+                  <img src=${photo.src.large} class="bd-placeholder-img card-img-top" />
+                  <div class="card-body">
+                    <h5 class="card-title">${photo.photographer}</h5>
+                    <p class="card-text">
+                    ${photo.alt}
+                    </p>
+                    <div class="d-flex justify-content-between align-items-center">
+                      <div class="btn-group">
+                        <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                        <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                      </div>
+                      <small class="text-muted">9 mins</small>
                     </div>
-                    <small class="text-muted">9 mins</small>
                   </div>
-                </div>
-              </div>`;
+                </div>`;
 
-      row.appendChild(col);
-    });
+        row.appendChild(col);
+      });
+    }
   })
   .catch((error) => console.error("Errore:", error));
