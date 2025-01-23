9ef61f3c4d0bda0d0c57adc534fe9397b82aac4c
Nicola Vettone <nicolavettone57@gmail.com>
2025-01-23 15:20:22 +0100

    APIesercizio

---
+++ script.js
@@ -0,0 +1,17 @@
+fetch("https://api.pexels.com/v1/search?query=hamsters", {
+  headers: {
+    Authorization: "Kk3phZEnBMs8AxnxcGTt1CYW4Ij70jyy7oyNe1lIHpy0xaXp077DcCKO",
+  },
+})
+  .then((response) => {
+    console.log(response);
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error("Errore nella richiesta: " + response.status);
+    }
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => console.error("Errore:", error));
